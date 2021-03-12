c0937900bf6fb5de2046655125ab9cf780594db9
Alec Scott <alec@bcs.sh>
2021-03-12 14:02:16 -0700

    Update Spec

--- spack/a/augustus/package.py
+++ spack/a/augustus/package.py
@@ -65,7 +65,11 @@ class Augustus(MakefilePackage):
                             'bamtools'].prefix)
             makefile.filter('INCLUDES = *',
                             'INCLUDES = -I$(BAMTOOLS)/include/bamtools ')
-            if 'bamtools@2.5:' in spec:
+            if 'bamtools^2.5.1' in spec:
+                makefile.filter('LIBS = -lbamtools -lz',
+                                'LIBS = $(BAMTOOLS)/lib'
+                                '/libbamtools.a -lz')
+            if 'bamtools@2.5.0' in spec:
                 makefile.filter('LIBS = -lbamtools -lz',
                                 'LIBS = $(BAMTOOLS)/lib64'
                                 '/libbamtools.a -lz')
@@ -79,6 +83,10 @@ class Augustus(MakefilePackage):
                             'BAMTOOLS = %s' % self.spec['bamtools'].prefix)
             makefile.filter('INCLUDES = /usr/include/bamtools',
                             'INCLUDES = $(BAMTOOLS)/include/bamtools')
+            if 'bamtools^2.5.1' in spec:
+                makefile.filter('LIBS = -lbamtools -lz',
+                                'LIBS = $(BAMTOOLS)/lib'
+                                '/libbamtools.a -lz')
             if 'bamtools@2.5:' in spec:
                 makefile.filter('LIBS = -lbamtools -lz',
                                 'LIBS = $(BAMTOOLS)/lib64'
