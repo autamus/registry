fd50dba2b943fbedcae9e44c11ba82ef859931f2
Alec Scott <alec@bcs.sh>
2021-03-16 18:31:07 -0700

    Rebuild Wget

--- spack/w/wget/package.py
+++ spack/w/wget/package.py
@@ -10,7 +10,7 @@ class Wget(AutotoolsPackage, GNUMirrorPackage):
     """GNU Wget is a free software package for retrieving files using
     HTTP, HTTPS and FTP, the most widely-used Internet protocols. It is a
     non-interactive commandline tool, so it may easily be called from scripts,
-    cron jobs, terminals without X-Windows support, etc."""
+    cron jobs, terminals without X-Windows support, etc.""" 
 
     homepage = "http://www.gnu.org/software/wget/"
     url      = "https://mirrors.rit.edu/gnu/wget/wget-1.21.1.tar.gz"
