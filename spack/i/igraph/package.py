19663647f9f682b2a0bbeb8fed72efa29fbeff51
Alec Scott <alec@bcs.sh>
2021-04-24 17:17:01 -0700

    Add missing dependencies

--- spack/i/igraph/package.py
+++ spack/i/igraph/package.py
@@ -14,6 +14,9 @@ class Igraph(AutotoolsPackage):
 
     version('0.9.2', sha256='fda86b5253daa3b994aaaa7aef0b8e4780dc8b2efbbdbf0aa71af9fedaecb073', url='https://github.com/igraph/igraph/releases/download/0.9.2/igraph-0.9.2.tar.gz')
     version('0.7.1', sha256='d978030e27369bf698f3816ab70aa9141e9baf81c56cc4f55efbe5489b46b0df')
-
+    
+    depends_on('automake', type='build')
+    depends_on('autoconf', type='build')
+    depends_on('libtool',  type='build')
     depends_on('libxml2')
     depends_on('m4')
