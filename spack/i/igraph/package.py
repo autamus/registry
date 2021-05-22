ea652bd09434dbb257dabeb4c4efc77e3183ad06
Alec Scott <alec@bcs.sh>
2021-05-21 23:15:52 -0700

    Add AutoTools Build Deps

--- spack/i/igraph/package.py
+++ spack/i/igraph/package.py
@@ -15,4 +15,8 @@ class Igraph(AutotoolsPackage):
     version('0.9.3', sha256='0cb185df3bdf16895c012e37c4a01b01e01a7b81f630df7602070765511eda87', url='https://github.com/igraph/igraph/releases/download/0.9.3/igraph-0.9.3.tar.gz')
     version('0.7.1', sha256='d978030e27369bf698f3816ab70aa9141e9baf81c56cc4f55efbe5489b46b0df')
 
+    depends_on('autoconf')
+    depends_on('automake')
+    depends_on('libtool')
     depends_on('libxml2')
+    depends_on('m4')
