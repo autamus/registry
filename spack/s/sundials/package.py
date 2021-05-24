1f5009eaf33a375fab35679f4c5ef06d29b57a56
Alec Scott <alec@bcs.sh>
2021-05-23 21:52:26 -0700

    Update Sundials

--- spack/s/sundials/package.py
+++ spack/s/sundials/package.py
@@ -13,8 +13,7 @@ class Sundials(CMakePackage, CudaPackage, ROCmPackage):
     Solvers)"""
 
     homepage = "https://computing.llnl.gov/projects/sundials"
-    urls = ["https://computing.llnl.gov/projects/sundials/download/sundials-2.7.0.tar.gz",
-            "https://github.com/LLNL/sundials/releases/download/v2.7.0/sundials-2.7.0.tar.gz"]
+    urls = "https://github.com/LLNL/sundials/releases/download/v2.7.0/sundials-2.7.0.tar.gz"
     git = "https://github.com/llnl/sundials.git"
     maintainers = ['cswoodward', 'gardner48', 'balos1']
 
