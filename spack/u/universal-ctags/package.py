8dac7985cff7087260ee1ff71794a790bb396197
Alec Scott <alecbcs@gmail.com>
2021-09-26 14:19:55 -0700

    Update package.py

--- spack/u/universal-ctags/package.py
+++ spack/u/universal-ctags/package.py
@@ -22,8 +22,6 @@ class UniversalCtags(AutotoolsPackage):
     version('5.9.20210912.0', sha256='5082d4f7e5695be3d697c46e2232d76c6d8adff51d22ba7a4b869362f444ee21')
     version('5.9.20210808.0', sha256='7f5f88d20750dfa2437ca9d163972b8684e3cf16de022a5177f322be92f528cc')
 
-
-
     depends_on('autoconf', type='build')
     depends_on('automake', type='build')
     depends_on('libtool',  type='build')
